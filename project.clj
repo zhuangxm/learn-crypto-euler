7796915d6964d235939b5a5d8836e55472fe326b
zxm <zxm@basecity.com>
2017-01-04 10:05:46 +0800

    add namespace dependency to project. solve week1.clj compile problem.

--- project.clj
+++ project.clj
@@ -4,6 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.clojure/tools.namespace "0.2.11"]
                  [clj-http "2.3.0"]
                  [org.clojure/math.combinatorics "0.1.3"]
                  [org.clojure/math.numeric-tower "0.0.4"]]
